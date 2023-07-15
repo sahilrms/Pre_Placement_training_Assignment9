1d5ec24c1a07fa531f2c04f2975b638665c1b3fc
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-07-16 00:09:03 +0530

    calculator ui fixed

--- Qno10/src/App.js
+++ Qno10/src/App.js
@@ -20,134 +20,134 @@ const APP = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-gray-100 rounded p-4">
-        <input
-          className="w-full p-2 mb-4 text-right"
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-950">
+      <div className="bg-black rounded p-4">
+       <div className="grid grid-cols-4 gap-2 h-12">
+         <input
+          className="col-span-3 p-2 mb-4 text-right "
           type="text"
           value={result}
           readOnly
         />
-        <div className="grid grid-cols-4 gap-2">
-          <button
-            className="col-span-2 bg-blue-500 hover:bg-blue-700 text-white rounded p-2"
+        <button
+            className="col-span-1 bg-gray-100 hover:bg-gray-400 text-black rounded  p-2 mb-4 text-right"
             name="clear"
             onClick={clear}
           >
             Clear
           </button>
+        </div>
+        <div className="col-span-1 grid grid-cols-3 gap-2">
+          
           <button
-            className="bg-yellow-500 hover:bg-yellow-700 text-white rounded p-2"
+            className="bg-orange-500 hover:bg-orange-700 text-white rounded p-2"
             name="="
             onClick={calculate}
           >
             =
           </button>
           <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="/"
-            onClick={handleClick}
-          >
-            /
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="7"
-            onClick={handleClick}
-          >
-            7
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="8"
-            onClick={handleClick}
-          >
-            8
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="9"
-            onClick={handleClick}
-          >
-            9
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="*"
-            onClick={handleClick}
-          >
-            *
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="4"
-            onClick={handleClick}
-          >
-            4
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="5"
-            onClick={handleClick}
-          >
-            5
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="6"
-            onClick={handleClick}
-          >
-            6
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="-"
-            onClick={handleClick}
-          >
-            -
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="1"
-            onClick={handleClick}
-          >
-            1
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="2"
-            onClick={handleClick}
-          >
-            2
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="3"
-            onClick={handleClick}
-          >
-            3
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
+            className="col-span-2 bg-orange-500 hover:bg-orange-700 text-white rounded p-2"
             name="+"
             onClick={handleClick}
           >
             +
           </button>
           <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
+            className="bg-orange-500 hover:bg-orange-700 text-white rounded p-2"
+            name="*"
+            onClick={handleClick}
+          >
+            *
+          </button>
+          <button
+            className="bg-orange-500 hover:bg-orange-700 text-white rounded p-2"
+            name="-"
+            onClick={handleClick}
+          >
+            -
+          </button>
+          <button
+            className="bg-orange-500 hover:bg-orange-700 text-white rounded p-2"
+            name="/"
+            onClick={handleClick}
+          >
+            /
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="1"
+            onClick={handleClick}
+          >
+            1
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="2"
+            onClick={handleClick}
+          >
+            2
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="3"
+            onClick={handleClick}
+          >
+            3
+          </button>
+          
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="4"
+            onClick={handleClick}
+          >
+            4
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="5"
+            onClick={handleClick}
+          >
+            5
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="6"
+            onClick={handleClick}
+          >
+            6
+          </button>
+          
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="7"
+            onClick={handleClick}
+          >
+            7
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="8"
+            onClick={handleClick}
+          >
+            8
+          </button>
+          <button
+            className="bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
+            name="9"
+            onClick={handleClick}
+          >
+            9
+          </button>
+          
+          <button
+            className="col-span-3 bg-gray-100 hover:bg-gray-300 text-black rounded p-2"
             name="0"
             onClick={handleClick}
           >
             0
           </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white rounded p-2"
-            name="."
-            onClick={handleClick}
-          >
-            .
-          </button>
+          
         </div>
       </div>
     </div>
